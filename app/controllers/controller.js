4f8147fbfac1f01e4375147effe038f0c5510fb2
Carlos Alexandre Leutz <54986289+Carlos-Alexandre-Leutz@users.noreply.github.com>
2020-11-04 13:59:40 -0300

    alexandre

---
+++ app/controllers/controller.js
@@ -0,0 +1,20 @@
+app.controller('HomeCtrl', function($scope) {
+    $scope.title = 'Bem-vindo, esta é nossa página principal!';
+});
+
+app.controller('nossaHistoriaCtrl', function($scope) {
+    $scope.title = 'nossa historia';
+});
+
+app.controller('nossoTrabalhoCtrl', function($scope) {
+    $scope.title = 'trabalho';
+});
+app.controller('produtosCtrl', function($scope) {
+    $scope.title = 'produtos';
+});
+app.controller('servicosCtrl', function($scope) {
+    $scope.title = 'serviços';
+});
+app.controller('contatoCtrl', function($scope) {
+    $scope.title = 'contato';
+});
---
+++ app/routes/route.js
@@ -0,0 +1,32 @@
+app.config(function($routeProvider, $locationProvider) {
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'app/views/home.html',
+            controller: 'HomeCtrl',
+        })
+        .when('/nosahistoria', {
+            templateUrl: 'app/views/nossaHistoria.html',
+            controller: 'nossaHistoriaCtrl',
+        })
+        .when('/nossotrabalho', {
+            templateUrl: 'app/views/nossoTrabalho.html',
+            controller: 'nossoTrabalhoCtrl',
+        })
+        .when('/produtos', {
+            templateUrl: 'app/views/produtos.html',
+            controller: 'produtosCtrl',
+        })
+        .when('/servicos', {
+            templateUrl: 'app/views/servicos.html',
+            controller: 'servicosCtrl',
+        })
+        .when('/faleconosco', {
+            templateUrl: 'app/views/contato.html',
+            controller: 'contatoCtrl',
+        })
+
+
+    .otherwise({ redirectTo: '/' });
+});
